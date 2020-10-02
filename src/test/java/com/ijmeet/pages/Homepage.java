d84e748e3a376de84610124013e6d3d1f6f4103c
deepakdude938 <deepakdude938@gmail.com>
2020-10-02 16:19:46 +0530

    Deepak commit

--- src/test/java/com/ijmeet/pages/Homepage.java
+++ src/test/java/com/ijmeet/pages/Homepage.java
@@ -1,5 +1,13 @@
 package com.ijmeet.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
+import com.ijmeet.util.Constant;
+
 public class Homepage {
+	public Homepage() {
+		PageFactory.initElements(Constant.driver, this);
+
+	}
 
 }
