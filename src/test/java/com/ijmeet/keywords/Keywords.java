191e9dad2c356c920b2cca5cf7c8b29f793720de
pooja deshpande <poojachimanpure10@gmail.com>
2020-10-06 00:04:46 +0530

    six oct changes

--- src/test/java/com/ijmeet/keywords/Keywords.java
+++ src/test/java/com/ijmeet/keywords/Keywords.java
@@ -13,26 +13,26 @@ import com.ijmeet.util.Constant;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Keywords {
-	public static void openBrowser(String browserName) {
+	public static void openBrowser(String browser) {
 
-		switch (browserName) {
-		case "chrome":
-			WebDriverManager.chromedriver().setup();
-			Constant.driver = new ChromeDriver();
-			break;
-		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
-			Constant.driver = new FirefoxDriver();
-			break;
-		case "IE":
-			WebDriverManager.iedriver().setup();
-			Constant.driver = new InternetExplorerDriver();
-			break;
+		switch(browser) 
+		{
+		     case "chrome":
+			 WebDriverManager.chromedriver().setup();
+			 Constant.driver = new ChromeDriver();
+			 break;
+		     case "firefox":
+			 WebDriverManager.firefoxdriver().setup();
+			 Constant.driver = new FirefoxDriver();
+			 break;
+		     case "IE":
+			 WebDriverManager.iedriver().setup();
+			 Constant.driver = new InternetExplorerDriver();
+			 break;
 		
-		default:
+		     default:
 			System.out.println("Open url successfully");
 		}
-	
-		
 	}
+	
 }
--- src/test/java/com/ijmeet/util/Constant.java
+++ src/test/java/com/ijmeet/util/Constant.java
@@ -1,13 +1,15 @@
 package com.ijmeet.util;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.FluentWait;
 
 public class Constant {
+	public final String url="http://www.ijmeet.com";
 	public static WebDriver driver;
-	public String url;
-
-	public static WebElement element;
+	public static List<WebElement> ele;
+    public static WebElement element;
 	public static FluentWait wait;
 }
