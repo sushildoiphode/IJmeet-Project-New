f6c55a5d2a5bce0729fb2a076b2451ffd540190e
pooja deshpande <poojachimanpure10@gmail.com>
2020-10-06 21:12:14 +0530

    changes made

--- SignUpTests.java
+++ SignUpTests.java
@@ -1,10 +1,3 @@
-package com.ijmeet.package1;
-
-import org.testng.annotations.Test;
-
-import com.ijmeet.keywords.Keywords;
-import com.ijmeet.pages.SignUppage;
-import com.ijmeet.util.Constant;
 
 public class SignUpTests {
 	@BeforeMethod
@@ -13,11 +6,6 @@ public class SignUpTests {
 		Keywords.openBrowser("chrome");
 		Constant.driver.get("https://ijmeet.com/register");
 		System.out.println("Sign up page  open successfully");
-	}
-	
-	public void closeBrowser()
-	{
-		
 	}
 
 }
