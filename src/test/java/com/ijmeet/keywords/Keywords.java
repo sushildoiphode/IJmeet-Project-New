5a7365ce1f3fd191b57b1ee36b84e1a93aac2d62
sushildoiphode <sushildoiphode@gmail.com>
2020-10-06 13:51:20 +0530

    Sushil Changes done

--- src/test/java/com/ijmeet/keywords/Keywords.java
+++ src/test/java/com/ijmeet/keywords/Keywords.java
@@ -1,6 +1,18 @@
 package com.ijmeet.keywords;
 
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -14,6 +26,9 @@ import org.openqa.selenium.support.ui.Select;
 import com.ijmeet.util.Constant;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 public class Keywords {
 	public static void openBrowser(String browserName) {
@@ -33,14 +48,14 @@ public class Keywords {
 			break;
 
 		default:
-			System.out.println("Open url successfully");
+			System.out.println("Invalid browser is selected");
 		}
 	}
 
 	public static void launchUrl(String url) {
 		Constant.driver.get(url);
 	}
-	
+
 	public static void clearCookies() {
 		Constant.driver.manage().deleteAllCookies();
 	}
@@ -48,15 +63,15 @@ public class Keywords {
 	public static void maximizeBrowser() {
 		Constant.driver.manage().window().maximize();
 	}
-	
+
 	public static void navigateBack() {
 		Constant.driver.navigate().back();
 	}
-	
+
 	public static void navigateForward() {
 		Constant.driver.navigate().forward();
 	}
-	
+
 	public static WebElement getWebElement(String locatorType, String locatorValue) {
 		WebElement element = null;
 		switch (locatorType) {
@@ -83,11 +98,8 @@ public class Keywords {
 		default:
 			System.err.println("Invalid Locator Type");
 		}
-
 		return element;
 	}
-	
-	
 
 	public static void enterText(String locatorType, String locatorValue, String textToEnter) {
 		getWebElement(locatorType, locatorValue).sendKeys(textToEnter);
@@ -106,20 +118,20 @@ public class Keywords {
 	public static boolean isElementDisplayed(String locatorType, String locatorValue) {
 		return getWebElement(locatorType, locatorValue).isDisplayed();
 	}
-	
+
 	public static void mouseHover(String locatorType, String locatorValue) {
 		WebElement element = getWebElement(locatorType, locatorValue);
 		System.out.println(element);
 		Actions act = new Actions(Constant.driver);
 		act.moveToElement(element).build().perform();
 	}
-	
+
 	public static String getTextOfElement(String locatorType, String locatorValue) {
 		WebElement element1 = getWebElement(locatorType, locatorValue);
-		String text=element1.getText();
+		String text = element1.getText();
 		return text;
-
 	}
+
 	public static List<WebElement> getListOfWebelements(String locatorType, String locatorValue) {
 		List<WebElement> element = null;
 		switch (locatorType) {
@@ -145,7 +157,67 @@ public class Keywords {
 			break;
 		}
 		return element;
+	}
 
+	public static void getScreenShot() {
+		String path = System.getProperty("user.dir");
+		File f = new File(path + "/Screenshot/" + getDate());
+
+		if (f.mkdirs() == true) {
+			System.out.println("Folder Created");
+		} else {
+			System.out.println("Folder not Created");
+		}
+		File file = new File(f, "/screenshot_" + getDateAndTime() + ".png");
+
+		try {
+			Robot robo = new Robot();
+			Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+			Rectangle rect = new Rectangle(d);
+			BufferedImage image = robo.createScreenCapture(rect);
+			ImageIO.write(image, "png", file);
+		} catch (AWTException e) {
+			System.err.println("unable to take Screenshot");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Unable to take Screenshot");
+			e.printStackTrace();
+		}
+	}
+
+	private static String getDateAndTime() {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy_MM_dd_HHmmss");
+		String formatteddate = now.format(date);
+		return formatteddate;
+		}
+
+	private static String getDate() {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy_MM_dd");
+		String formattedDate1 = now.format(date);
+		return formattedDate1;
+		}
+
+	
+	
+	public  void fullPageScreenShot() {
+		String path = System.getProperty("user.dir");
+		File f = new File(path + "/Screenshot/" + getDate());
+
+		if (f.mkdirs() == true) {
+			System.out.println("Folder Created");
+		} else {
+			System.out.println("Folder not Created");
+		}
+		File file = new File(f, "/screenshot_" + getDateAndTime() + ".png");
+		AShot ashot=new AShot();
+		Screenshot sc=ashot.shootingStrategy(ShootingStrategies.viewportPasting(2000)).takeScreenshot(Constant.driver);
+		try {
+			ImageIO.write(sc.getImage(), "PNG", file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static int displayListCount(String locatorType, String locatorValue) {
