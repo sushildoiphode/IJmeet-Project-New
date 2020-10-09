472babac0b3791f0550ea6a06d9b80f17bc7a56a
SampadaBhujbal <sampada96bhujbal@gmail.com>
2020-10-09 14:49:45 +0530

    TC of homepage

--- src/test/java/com/ijmeet/pages/HomePage.java
+++ src/test/java/com/ijmeet/pages/HomePage.java
@@ -10,34 +10,5 @@ public class HomePage {
 	public HomePage() {
 		PageFactory.initElements(Constant.driver, this);
 	}
-	@FindBy(xpath="//a[contains(text(),'Join Meeting')]")	
-	public WebElement joinMeeting;
-
-	@FindBy(xpath="//a[contains(text(),'Host Meeting')]")
-	public WebElement hostMeeting;
-
-	@FindBy(xpath="//a[contains(text(),'Contact sales')]")
-	public WebElement contactSales;
-
-	@FindBy(xpath="//a[contains(text(),'Sign In')]")
-	public WebElement signIn;
-
-	@FindBy(xpath="//a[contains(text(),'Sign Up')]")
-	public WebElement signUp;
-
-	public void clickOnJoinMeeting() {
-		joinMeeting.click();
-	}
-	public void clickOnHostMeeting() {
-		hostMeeting.click();
-	}
-	public void clickcOntactSales() {
-		contactSales.click();
-	}
-	public void clickOnSignIn() {
-		signIn.click();
-	}
-	public void clickOnSignUp() {
-		signUp.click();
-	}
+	
 }
